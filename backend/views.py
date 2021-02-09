3121b39215bca4a837f4acaa8d13c60d19d0777c
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-02-09 13:34:30 +0300

    added more views

--- backend/views.py
+++ backend/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'backend/index.html')
+
+
+def about(request):
+    return render(request, 'backend/about.html')
+
+
+def products(request):
+    return render(request, 'backend/products.html')
+
+
+def store(request):
+    return render(request, 'backend/store.html')
