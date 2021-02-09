59ba9aab198c4cff0658cc70a88e03f6253ee955
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-02-09 13:40:17 +0300

    more urls

--- backend/urls.py
+++ backend/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('products/', views.products, name='products'),
+    path('store/', views.store, name='store'),
 ]
