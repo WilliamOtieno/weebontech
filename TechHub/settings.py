31fc5d932c8ef3e31609cfdcdf14c68e5aa1fa7e
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-02-14 18:29:30 +0300

    final commit

--- TechHub/settings.py
+++ TechHub/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'TechHub.urls'
@@ -122,3 +123,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = {
+    os.path.join(BASE_DIR, 'static'),
+}
