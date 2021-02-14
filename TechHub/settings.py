061570686df533c287738159a9651fb80efe1c3d
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-02-14 18:32:24 +0300

    final commit

--- TechHub/settings.py
+++ TechHub/settings.py
@@ -124,6 +124,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = {
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-}
+)
